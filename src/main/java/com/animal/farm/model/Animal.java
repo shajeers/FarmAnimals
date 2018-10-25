a6275ef2453c3380b5283ebbb21d2461de1df968
U37938 <U37938@F989DTRV.ustr.com>
2018-10-25 17:30:55 +0530

    Code Formatted

--- src/main/java/com/animal/farm/model/Animal.java
+++ src/main/java/com/animal/farm/model/Animal.java
@@ -1,73 +1,53 @@
 package com.animal.farm.model;
 
 public abstract class Animal {
-	
-	public static final String NOT_SUPPORTED="Not Supported";
-	
-	public boolean canWalk()
-	{
+
+	public static final String NOT_SUPPORTED = "Not Supported";
+
+	public boolean canWalk() {
 		return true;
 	}
-	
-	public boolean canFly()
-	{
+
+	public boolean canFly() {
 		return false;
 	}
 
-	public boolean canSwim()
-	{
+	public boolean canSwim() {
 		return false;
 	}
-	
-	public boolean canMakeSound()
-	{
+
+	public boolean canMakeSound() {
 		return true;
 	}
-	
-	public String sound()
-	{
-		if(canMakeSound())
-		{
+
+	public String sound() {
+		if (canMakeSound()) {
 			return "Animal Sound";
-		}
-		else
-		{
+		} else {
 			return NOT_SUPPORTED;
 		}
 	}
-	
-	public String soundSecondLang()
-	{
-		if(canMakeSound())
-		{
+
+	public String soundSecondLang() {
+		if (canMakeSound()) {
 			return "Mrigham Ocha";
-		}
-		else
-		{
+		} else {
 			return NOT_SUPPORTED;
 		}
 	}
-	
-	public String walk()
-	{
-		if(canWalk())
-		{
+
+	public String walk() {
+		if (canWalk()) {
 			return "Animal walking";
-		}
-		else
-		{
+		} else {
 			return NOT_SUPPORTED;
 		}
 	}
-	
-	public String fly()
-	{
-		if(canFly())
-		{
+
+	public String fly() {
+		if (canFly()) {
 			return "Animal flying";
-		}
-		else
-		{
+		} else {
 			return NOT_SUPPORTED;
 		}
 	}
